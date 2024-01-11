968f9e850123fad361584f2b12bb526da118e4b4
jivanshupoplifp <156290515+jivanshupoplifp@users.noreply.github.com>
2024-01-11 22:34:38 +0530

    Delete 10.2.2024 directory

--- 10.2.2024/IEmployee.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace I{
-    public interface IEmployee{
-        public void AcceptDetails();
-        public void CalculateSalary();
-        public void DisplayDetails();
-    }
-}
