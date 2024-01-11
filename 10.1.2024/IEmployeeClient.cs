fcef7595b88e28c66b8464e83b0323ae53810540
jivanshupoplifp <156290515+jivanshupoplifp@users.noreply.github.com>
2024-01-11 22:33:26 +0530

    Add files via upload

---
+++ 10.1.2024/IEmployeeClient.cs
@@ -0,0 +1,20 @@
+namespace I{
+    class IEmployeeClient{
+
+        public static void main(){
+            Employee e1 = new Permenant();
+            Employee e2 = new Trainee();
+            Console.WriteLine("Enter details of Permenant Employee: ");
+            e1.AcceptDetails();
+            e1.CalculateSalary();
+            Console.WriteLine("Permenant Employee details: ");
+            e1.DisplayDetails();
+            Console.WriteLine("Enter details of Trainee: ");
+            e2.AcceptDetails();
+            e2.CalculateSalary();
+            Console.WriteLine("Trainee details: ");
+            e2.DisplayDetails();
+        }
+
+    }
+}
